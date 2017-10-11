5dcb15fe2a8a3d092c4e3540c0daa6fcfef14f39
Oleg <oleg.pav.m@gmail.com>
2017-10-12 00:34:15 +0300

    Basic landing.

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 
 gem 'sinatra'
+gem 'recursive-open-struct'
--- main.rb
+++ main.rb
@@ -1,2 +1,21 @@
 require 'sinatra'
+require 'recursive-open-struct'
 
+$locale = {}
+
+Dir['./locale/*'].each do |path|
+	name = path.split('/').last.split('.').first
+	hash = JSON.parse(File.read(path))
+
+	$locale[name] = RecursiveOpenStruct.new(hash)
+end
+
+get '/' do
+	redirect '/en'
+end
+
+get '/:lang' do
+	locale = $locale[params['lang']] or $locale['en']
+
+	erb(File.read('views/main.html'), locals: {l: locale})
+end
